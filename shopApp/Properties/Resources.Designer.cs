415622ac7696089def4eb72700d63bb0fe094bf5
Felipe Alamos <felipealamos@gmail.com>
2014-11-28 01:41:37 +0800

    Change assembly name

--- shopApp/Properties/Resources.Designer.cs
+++ shopApp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace imseWCard2.Properties {
+namespace ShopApp.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace imseWCard2.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("imseWCard2.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ShopApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- shopApp/Properties/Settings.Designer.cs
+++ shopApp/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace imseWCard2.Properties {
+namespace ShopApp.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
